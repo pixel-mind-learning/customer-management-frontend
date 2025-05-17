4e72e1c15d2166b1faa47efcdef8d707de6c4c21
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-05-17 19:13:49 +0530

    customer form developments

--- src/component/customer/Customer.jsx
+++ src/component/customer/Customer.jsx
@@ -31,16 +31,51 @@ export default function Customer() {
         customerHasMobileNumberRequest: [],
     });
 
+    const [addresses, setAddresses] = useState([
+        {
+            type: 'Home',
+            street: '123 Main St',
+            city: 'Colombo',
+            state: 'Western Province',
+            postalCode: '00100',
+            country: 'Sri Lanka'
+        }
+    ]);
+
+    const [mobileNumers, setMobileNumbers] = useState([
+        {
+            mobileNumer: '0770437288'
+        }
+    ]);
+
+    const [dependants, setDependants] = useState([
+        {
+            name: '0770437288',
+            dateOfBirth: '1999-01-02',
+            nic: '19950232140',
+            relationshipType: 'Spouse',
+            customerHasAddressRequest: [],
+            customerHasMobileNumberRequest: [],
+        }
+    ]);
+
+    const [selectedDateTime, setSelectedDateTime] = useState(null);
+
+    const handleDateTimeChange = (newValue) => {
+        setSelectedDateTime(newValue);
+        console.log('Selected DateTime:', newValue?.format());
+    };
+
     const roles = ['Admin', 'Customer', 'Manager'];
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0); // Reset page when rows per page changes
+        setPage(0);
     };
 
     const [customers, setCustomers] = React.useState([
@@ -59,13 +94,6 @@ export default function Customer() {
         // You can send this data to your backend here
     };
 
-    const [selectedDateTime, setSelectedDateTime] = useState(null); // initialize as null
-
-    const handleDateTimeChange = (newValue) => {
-        setSelectedDateTime(newValue);
-        console.log('Selected DateTime:', newValue?.format());
-    };
-
     return (
         <Paper
             elevation={3}
@@ -82,6 +110,7 @@ export default function Customer() {
                 Create Customer Form
             </Typography>
 
+            {/* Customer form started */}
             <Box
                 component="form"
                 onSubmit={handleSubmit}
@@ -103,40 +132,227 @@ export default function Customer() {
                         fullWidth
                         label="Date of Birth"
                         value={selectedDateTime}
-                        onChange={handleChange}
+                        onChange={handleDateTimeChange}
                         slotProps={{ textField: { fullWidth: true, required: true, margin: 'normal' } }}
                         renderInput={(params) => <TextField {...params} />}
+                        required
                     />
                 </LocalizationProvider>
                 <TextField
                     fullWidth
-                    label="Password"
-                    name="password"
-                    type="password"
-                    value={formData.password}
+                    label="NIC"
+                    name="NIC"
+                    value={formData.nic}
                     onChange={handleChange}
                     margin="normal"
                     required
                 />
-                <TextField
-                    select
-                    fullWidth
-                    label="Role"
-                    name="role"
-                    value={formData.role}
-                    onChange={handleChange}
-                    margin="normal"
-                    required
-                >
-                    {roles.map((role) => (
-                        <MenuItem key={role} value={role}>
-                            {role}
-                        </MenuItem>
-                    ))}
-                </TextField>
+
+                {/* Customer Location info form started */}
+                <Typography variant="h6" sx={{ mt: 3 }}>Customer Location Info.</Typography>
+
+                {addresses.map((address, index) => (
+                    <Box key={index} sx={{ border: '1px solid #ddd', borderRadius: 2, p: 2, mt: 2 }}>
+                        <Typography variant="subtitle1">Location {index + 1}</Typography>
+                        <TextField
+                            fullWidth
+                            label="Address Line 1"
+                            name="addressLine1"
+                            value={address.addressLine1}
+                            margin="normal"
+                            required
+                        />
+                        <TextField
+                            fullWidth
+                            label="Address Line 2"
+                            name="addressLine2"
+                            value={address.addressLine2}
+                            margin="normal"
+                            required
+                        />
+                        <TextField
+                            select
+                            fullWidth
+                            label="City"
+                            name="cityId"
+                            value={address.cityId}
+                            margin="normal"
+                            required
+                        >
+                            {roles.map((role) => (
+                                <MenuItem key={role} value={role}>
+                                    {role}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            select
+                            fullWidth
+                            label="Country"
+                            name="countryId"
+                            value={address.countryId}
+                            margin="normal"
+                            required
+                        >
+                            {roles.map((role) => (
+                                <MenuItem key={role} value={role}>
+                                    {role}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </Box>
+                ))}
+
+                <Button sx={{ mt: 2 }} variant="outlined">
+                    Add Another Address
+                </Button>
+
+                {/* Contact info form started */}
+                <Typography variant="h6" sx={{ mt: 3 }}>Customer Contact Info.</Typography>
+
+                {mobileNumers.map((mobileNo, index) => (
+                    <Box key={index} sx={{ border: '1px solid #ddd', borderRadius: 2, p: 2, mt: 2 }}>
+                        <Typography variant="subtitle1">Contact {index + 1}</Typography>
+                        <TextField
+                            fullWidth
+                            label="Mobile No"
+                            name="mobileNumber"
+                            value={mobileNo.mobileNumer}
+                            margin="normal"
+                            required
+                        />
+                    </Box>
+                ))}
+
+                <Button sx={{ mt: 2 }} variant="outlined">
+                    Add Another Contact
+                </Button>
+                {/* Customer Location info form ended */}
+
+                {/* Dependant form started */}
+                <Typography variant="h6" sx={{ mt: 3 }}>Dependant Info.</Typography>
+
+                {dependants.map((dependant, index) => (
+                    <Box key={index} sx={{ border: '1px solid #ddd', borderRadius: 2, p: 2, mt: 2 }}>
+                        <Typography variant="subtitle1">Dependant {index + 1}</Typography>
+                        <TextField
+                            fullWidth
+                            label="Name"
+                            name="name"
+                            value={dependant.name}
+                            margin="normal"
+                            required
+                        />
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DateTimePicker
+                                fullWidth
+                                label="Date of Birth"
+                                value={selectedDateTime}
+                                onChange={handleDateTimeChange}
+                                slotProps={{ textField: { fullWidth: true, required: true, margin: 'normal' } }}
+                                renderInput={(params) => <TextField {...params} />}
+                                required
+                            />
+                        </LocalizationProvider>
+                        <TextField
+                            fullWidth
+                            label="NIC"
+                            name="NIC"
+                            value={formData.nic}
+                            onChange={handleChange}
+                            margin="normal"
+                            required
+                        />
+
+                        {/* Dependant Location info form started */}
+                        <Typography variant="h6" sx={{ mt: 3 }}>Dependant Location Info.</Typography>
+
+                        {addresses.map((address, index) => (
+                            <Box key={index} sx={{ border: '1px solid #ddd', borderRadius: 2, p: 2, mt: 2 }}>
+                                <Typography variant="subtitle1">Location {index + 1}</Typography>
+                                <TextField
+                                    fullWidth
+                                    label="Address Line 1"
+                                    name="addressLine1"
+                                    value={address.addressLine1}
+                                    margin="normal"
+                                    required
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Address Line 2"
+                                    name="addressLine2"
+                                    value={address.addressLine2}
+                                    margin="normal"
+                                    required
+                                />
+                                <TextField
+                                    select
+                                    fullWidth
+                                    label="City"
+                                    name="cityId"
+                                    value={address.cityId}
+                                    margin="normal"
+                                    required
+                                >
+                                    {roles.map((role) => (
+                                        <MenuItem key={role} value={role}>
+                                            {role}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                                <TextField
+                                    select
+                                    fullWidth
+                                    label="Country"
+                                    name="countryId"
+                                    value={address.countryId}
+                                    margin="normal"
+                                    required
+                                >
+                                    {roles.map((role) => (
+                                        <MenuItem key={role} value={role}>
+                                            {role}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </Box>
+                        ))}
+
+                        <Button sx={{ mt: 2 }} variant="outlined">
+                            Add Another Address
+                        </Button>
+
+                        {/* Dependant Contact info form started */}
+                        <Typography variant="h6" sx={{ mt: 3 }}>Dependant Contact Info.</Typography>
+
+                        {mobileNumers.map((mobileNo, index) => (
+                            <Box key={index} sx={{ border: '1px solid #ddd', borderRadius: 2, p: 2, mt: 2 }}>
+                                <Typography variant="subtitle1">Contact {index + 1}</Typography>
+                                <TextField
+                                    fullWidth
+                                    label="Mobile No"
+                                    name="mobileNumber"
+                                    value={mobileNo.mobileNumer}
+                                    margin="normal"
+                                    required
+                                />
+                            </Box>
+                        ))}
+
+                        <Button sx={{ mt: 2 }} variant="outlined">
+                            Add Another Contact
+                        </Button>
+
+                        {/* Dependant Location info form ended */}
+                    </Box>
+
+                ))}
+
                 <Button type="submit" variant="contained" sx={{ mt: 3 }} fullWidth>
                     Create Customer
                 </Button>
+                {/* Customer form ended */}
             </Box>
 
             {/* Table for customer details */}
